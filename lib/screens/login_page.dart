57491c142ef6f1f74c5d51e7f99c06ddd7e60ef1
Dominik Roszkowski <kontakt@dominikroszkowski.pl>
2019-03-21 22:17:46 +0100

    Add simple login page

--- lib/screens/login_page.dart
+++ lib/screens/login_page.dart
@@ -1,1 +1,72 @@
-// final userAvatar = 'http://lorempixel.com/64/64/cats/${math.Random().nextInt(11)}/';
+import 'package:flutter/material.dart';
+import 'dart:math' as math;
+import 'package:cached_network_image/cached_network_image.dart';
+
+class LoginPage extends StatefulWidget {
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final userAvatar = 'http://lorempixel.com/200/200/cats/${math.Random().nextInt(10)}/';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        padding: EdgeInsets.symmetric(horizontal: 40),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            CircleAvatar(
+              backgroundImage: CachedNetworkImageProvider(userAvatar),
+              radius: 50,
+            ),
+            SizedBox(height: 40),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 8),
+              child: TextField(
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(40),
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide.none,
+                      borderRadius: BorderRadius.circular(40),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide.none,
+                      borderRadius: BorderRadius.circular(40),
+                    ),
+                    fillColor: Colors.white,
+                    hintText: 'user',
+                    filled: true),
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            SizedBox(
+              width: double.infinity,
+              child: FlatButton(
+                padding: EdgeInsets.all(20),
+                color: Colors.lightBlue,
+                child: Text(
+                  'LOGIN',
+                  style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.w400,
+                    color: Colors.white,
+                  ),
+                ),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(40),
+                ),
+                onPressed: () {},
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
